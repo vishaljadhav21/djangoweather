2aa33daa47b9a73872905c98220a52044a1696c6
vishal jadhav <vhjadhav99@gmail.com>
2021-04-23 17:12:34 +0530

    Update manage.py
    doesn't change anything

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -16,6 +17,10 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+    
 
 if __name__ == '__main__':
     main()
+    
+    
+    
